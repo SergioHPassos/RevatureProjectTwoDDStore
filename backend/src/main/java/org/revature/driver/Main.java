dfefa81a10fa206c8fc9b56cbabab1bf1de34b8b
SergioHPassos <shp360@hotmail.com>
2022-11-10 18:41:52 -0500

    comment

--- backend/src/main/java/org/revature/driver/Main.java
+++ backend/src/main/java/org/revature/driver/Main.java
@@ -3,6 +3,7 @@ package org.revature.driver;
 import io.javalin.Javalin;
 
 public class Main {
+    // main entry point
     public static void main(String[] args) {
         // instance Javalin server object
         Javalin app = Javalin.create();
