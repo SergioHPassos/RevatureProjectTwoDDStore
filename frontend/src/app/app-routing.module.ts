4684665c9eb22c33bb3f7fd945bd3644b91712b1
Conro101 <cbot.596@gmail.com>
2022-11-10 15:56:56 -0500

    initial work on services, models and routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { FrontComponent } from './components/front/front.component';
+import { ProductComponent } from './components/product/product.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component: FrontComponent},
+  {path:"product", component: ProductComponent},
+  {path:"cart", component: CartComponent},
+  {path:"user", component:UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontComponent } from './front/front.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontComponent,
+    ProductComponent,
+    CartComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
