924c98a7d523b24defebbd2fb4e631e36bfbb105
SergioHPassos <shp360@hotmail.com>
2022-11-10 18:27:01 -0500

    database connection with heroku works, basic database testing completed

--- backend/src/main/java/org/revature/driver/DBConnection.java
+++ backend/src/main/java/org/revature/driver/DBConnection.java
@@ -5,16 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+    // instance variables
     public static Connection connection;
+    private static String url = "jdbc:postgresql://ec2-35-170-146-54.compute-1.amazonaws.com/d9lfkfb8q1efj7";
+    private static String user = "xquekqkbufprxs";
+    private static String password = "6adaf7b25ec1c3cb26c3e1c353a98a3455e7cd43a5f820a98fa852cd50ee03e4";
 
     public static Connection getConnection() {
-        // String url = System.getenv("POSTGRES_SQL_DB");
-        // String username = System.getenv("DB_USERNAME");
-        // String password = System.getenv("PASSWORD");
+
+
+
         try {
-            // create connection object, make connection with postgres db, return connection object
-            // connection = DriverManager.getConnection(url, username, password);
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres?user=postgres&password=password");
+            connection = DriverManager.getConnection(url, user, password);
             return connection;
         } catch (SQLException e) {
             e.printStackTrace();
--- backend/src/test/java/UtilsTest/ConnectionTest.java
+++ backend/src/test/java/UtilsTest/ConnectionTest.java
@@ -1,5 +1,18 @@
 package UtilsTest;
 
-public class ConnectionTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.revature.driver.DBConnection;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionTest {
+    @Test
+    void connection_available() throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        System.out.println(connection);
+        Assertions.assertNotNull(connection);
+        connection.close();
+    }
 }
