33c94196182b725d1820f4da846538d7be3bfa94
Joshua Otero <jotero1022@gmail.com>
2022-11-15 12:31:02 -0500

    CHANGED: Testing stystem print line for readability.

--- backend/src/test/java/DAOtests/ProductTests.java
+++ backend/src/test/java/DAOtests/ProductTests.java
@@ -17,7 +17,7 @@ public class ProductTests {
     @Order(1)
     void get_all_products_test() {
         ArrayList<Products> products = productDAOMock.getProducts();
-        System.out.println(products.get(1).toString());
+        System.out.println("Test 1: "+ products.get(1).toString());
         Assertions.assertTrue(products.size() > 2);
     }
 
@@ -26,27 +26,27 @@ public class ProductTests {
     void get_products_by_type_test() {
         ArrayList<Products> swords = productDAOMock.getProductsbyType("Swords");
         ArrayList<Products> potions = productDAOMock.getProductsbyType("Potions");
-        System.out.println(swords.get(0).toString());
-        System.out.println(potions.get(0).toString());
+        System.out.println("Test 2: "+swords.get(0).toString());
+        System.out.println("Test 2: "+potions.get(0).toString());
         Assertions.assertFalse(swords.get(0) == potions.get(0));
     }
 
     @Test
-    @Order(2)
+    @Order(3)
     void get_products_by_type_and_subtype_test() {
         ArrayList<Products> swords = productDAOMock.getProductsbyTypeAndSubtype("Swords", "Longswords");
         ArrayList<Products> shortsword = productDAOMock.getProductsbyTypeAndSubtype("Swords", "Shortswords");
-        System.out.println(swords.get(0).toString());
-        System.out.println(shortsword.get(0).toString());
+        System.out.println("Test 3: "+swords.get(0).toString());
+        System.out.println("Test 3: "+shortsword.get(0).toString());
         Assertions.assertFalse(swords.get(0) == shortsword.get(0));
     }
 
     @Test
-    @Order(3)
+    @Order(4)
     void get_products_by_id_test() {
         // Works as long as id 1 belongs to Longsword
         ArrayList<Products> swords = productDAOMock.getProductsbyID(1);
-        System.out.println(swords.get(0).toString());
+        System.out.println("Test 4: "+swords.get(0).toString());
         Assertions.assertTrue(swords.get(0).getName().equals("Longsword"));
     }
 
