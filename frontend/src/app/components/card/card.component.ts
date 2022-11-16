9622e181e9de9a69109f011343bad8d366728d4e
KubanD936 <kuban627@revature.net>
2022-11-16 12:48:06 -0500

    Added inputs to card component in ts and html

--- frontend/src/app/components/card/card.component.ts
+++ frontend/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -11,5 +11,6 @@ export class CardComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  @Input() inputProduct: any = null;
 
 }
