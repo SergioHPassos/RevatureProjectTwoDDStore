266ca62ed61dcda38ae68f1e8819dd980176fde0
KubanD936 <kuban627@revature.net>
2022-11-17 14:57:16 -0500

    Finished buttons, types and subtypes

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -10,14 +10,16 @@ export class SidebarComponent implements OnInit {
     type: {
       sword: 'SWORD',
       armour: 'ARMOUR',
-      shield: 'SHIELD',
       potion: 'POTION',
+      shield: 'SHIELD',
+      travel: 'TRAVEL'
     },
     subtypes: {
       sword: ['DAGGER', 'SHORT', 'LONG', 'RAPIER', 'TWO-HANDED', 'GIANT ONLY'],
-      armour: ['HEAVY', 'MEDIUM', 'LIGHT', 'MAGIC ROBES', 'GIANT ONLY'],
-      shield: [ 'BUCKLER','KITE', 'HEATER', 'TOWER','GIANT ONLY'],
-      potion: ['HEALING', 'MANA', 'BUFF'],
+      armour: ['MAGIC ROBES', 'LIGHT', 'MEDIUM', 'HEAVY', 'GIANT ONLY'],
+      shield: [ 'BUCKLER', 'TARGE', 'HEATER', 'KITE',  'TOWER','GIANT ONLY'],
+      potion: ['HEALING', 'MANA', 'BUFF', 'UTILITY'],
+      travel: ['CLOTHING', 'FOOD', 'SUPPLIES']
     },
   };
 
