028115019aeadece976bcbcea62a75b29fb73b6e
SergioHPassos <shp360@hotmail.com>
2022-11-17 00:03:50 -0500

    reworking sidebar component

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  types = {
+    type: {
+      sword: 'SWORD',
+      shield: 'SHIELD',
+      potion: 'POTION',
+    },
+    subtypes: {
+      sword: ['DAGGER', 'SHORT', 'LONG'],
+      shield: ['USELESS', 'LARGE', 'GIANT ONLY'],
+      potion: ['HEALING', 'MANA', 'BUFF'],
+    },
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
