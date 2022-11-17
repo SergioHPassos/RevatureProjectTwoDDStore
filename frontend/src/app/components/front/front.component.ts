2d2099e93b763dcfdf324d93a74824c5444bbf7b
Conro101 <cbot.596@gmail.com>
2022-11-17 14:54:09 -0500

    replaced changes lost in merge

--- frontend/src/app/components/front/front.component.ts
+++ frontend/src/app/components/front/front.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,13 +10,47 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class FrontComponent implements OnInit {
 
-  constructor(private productService: ProductService, private router: Router) { }
+  constructor(private productService: ProductService, private router: Router, private activatedRoute:ActivatedRoute) { }
 
   products: Product[] = [];
+  requestedType: any = this.activatedRoute.snapshot.paramMap.get('type');
+  requestedSubtype: any = this.activatedRoute.snapshot.paramMap.get('subtype');
 
   ngOnInit(): void {
     (async () => {
-      this.products = await this.productService.getAllProducts();
+      if(this.requestedSubtype !== null){
+        const product: Product = {
+          id: 0,
+          name: "",
+          type: this.requestedType,
+          subtype: this.requestedSubtype,
+          price: 0,
+          image: "",
+          stock: 0,
+          rarity: "",
+          cartQuant: 0,
+          description: ""
+        };
+        this.products = await this.productService.getProductBySubtype(product);
+      }
+      else if(this.requestedType !== null){
+        const product: Product = {
+          id: 0,
+          name: "",
+          type: this.requestedType,
+          subtype: this.requestedSubtype,
+          price: 0,
+          image: "",
+          stock: 0,
+          rarity: "",
+          cartQuant: 0,
+          description: ""
+        };
+        this.products = await this.productService.getProductByType(product);
+      }
+      else{
+        this.products = await this.productService.getAllProducts();
+      }
     })
   }
 
