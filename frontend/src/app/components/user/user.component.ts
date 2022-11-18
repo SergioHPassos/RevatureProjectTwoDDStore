6b86cd19a5382ca5425f13743eac427069ce2bb4
SergioHPassos <shp360@hotmail.com>
2022-11-18 06:27:19 -0500

    basic profile injection working

--- frontend/src/app/components/user/user.component.ts
+++ frontend/src/app/components/user/user.component.ts
@@ -6,30 +6,39 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-
-  constructor(private userService:UserService, private router: Router) { }
+  constructor(private userService: UserService, private router: Router) {}
 
   currentUser: any = null;
   id: number = 0;
-  username: string = "";
-  password: string = "";
-  address: string = "";
+  username: string = '';
+  password: string = '';
+  address: string = '';
 
   ngOnInit(): void {
-    (async () => {
+    async () => {
       this.currentUser = await this.userService.getCurrentUser();
       this.username = this.currentUser.username;
       this.address = this.currentUser.address;
-    })
+    };
   }
 
-  async updateUser(){
-    const user: User = {id:this.id, username: this.username, password: this.password, address: this.address};
+  async updateUser() {
+    const user: User = {
+      id: this.id,
+      username: this.username,
+      password: this.password,
+      address: this.address,
+    };
     const gotUser: User = await this.userService.updateUser(user);
     this.address = gotUser.address;
   }
 
+  getCurrentUser = async () => {
+    this.currentUser = await this.userService.getCurrentUser();
+    this.username = this.currentUser.username;
+    this.address = this.currentUser.address;
+  };
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -4,37 +4,38 @@ import { firstValueFrom } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http : HttpClient) { }
-
-  async getCurrentUser():Promise<User>{
-    const observable = this.http.get<User>("placeholder");
+  async getCurrentUser(): Promise<User> {
+    const observable = this.http.get<User>(
+      'http://localhost:8080/getCurrentUser'
+    );
     const user = await firstValueFrom(observable);
     return user;
   }
 
-  async loginUser(user: User):Promise<User>{
-    const observable = this.http.post<User>("placeholder for login", user);
+  async loginUser(user: User): Promise<User> {
+    const observable = this.http.post<User>('placeholder for login', user);
     const foundUser = await firstValueFrom(observable);
     return foundUser;
   }
 
-  async registerUser(user: User):Promise<User>{
-    const observable = this.http.post<User>("placeholder for register", user);
+  async registerUser(user: User): Promise<User> {
+    const observable = this.http.post<User>('placeholder for register', user);
     const foundUser = await firstValueFrom(observable);
     return foundUser;
   }
 
-  async updateUser(user: User):Promise<User>{
-    const observable = this.http.post<User>("placeholder for update", user);
+  async updateUser(user: User): Promise<User> {
+    const observable = this.http.post<User>('placeholder for update', user);
     const foundUser = await firstValueFrom(observable);
     return foundUser;
   }
 
-  async logoutUser(){
-    const observable = this.http.get<string>("placeholder for logout");
+  async logoutUser() {
+    const observable = this.http.get<string>('placeholder for logout');
   }
 }
